6022cdb0d2de0943bd4afac4eb7ca733011655d0
wafaez <130220863+wafaez@users.noreply.github.com>
2023-04-08 11:15:53 +0000

    Add files via upload

---
+++ app/src/androidTest/java/com/mytracker/familygpstracker/ExampleInstrumentedTest.java
@@ -0,0 +1,26 @@
+package com.mytracker.familygpstracker;
+
+import android.content.Context;
+import androidx.test.platform.app.InstrumentationRegistry;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.*;
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * @see <app_bar_main href="http://d.android.com/tools/testing">Testing documentation</app_bar_main>
+ */
+@RunWith(AndroidJUnit4.class)
+public class ExampleInstrumentedTest {
+    @Test
+    public void useAppContext() {
+        // Context of the app under test.
+        Context appContext = InstrumentationRegistry.getTargetContext();
+
+        assertEquals("com.haroonstudios.familytracklocator", appContext.getPackageName());
+    }
+}
